a8511b5a9223617f28ce1151ae6cf2f8fcaff32e
GaryMuvaut <webdesign.bydgoszcz@gmail.com>
2023-11-04 19:38:08 +0100

    Dodanie śniegu i mechanizmu ukrywania przycisku losowania

--- wigilia-2023/src/app/draw/draw.component.ts
+++ wigilia-2023/src/app/draw/draw.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-draw',
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./draw.component.scss']
 })
 export class DrawComponent {
+
+  @ViewChild('coin') coin!: ElementRef;
+  coinClass!: string;
+  isDrawn: boolean = false;
+
+  coinFlip() {
+    var flipResult = Math.random();
+
+    this.coinClass = 'dupa';
+
+    setTimeout(() => {
+      if(flipResult <= 0.5){
+        this.coinClass = 'heads';
+        console.log('it is head');
+      }
+      else{
+        this.coinClass = 'tails';
+        console.log('it is tails');
+      }
+    }, 100);
+
+    setTimeout(() => {
+      this.isDrawn = true;
+    }, 3100);
+
+    // setTimeout(function(this){
+    //   if(flipResult <= 0.5){
+    //     this.coin.nativeElement.addClass('heads');
+    //     console.log('it is head');
+    //   }
+    //   else{
+    //     $('#coin').addClass('tails');
+    //     console.log('it is tails');
+    //   }
+    // }, 100);
+  };
 
 }
